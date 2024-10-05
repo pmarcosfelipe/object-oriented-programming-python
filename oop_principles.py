9ba5b2d893373b7449161871c4b35aee4a5c89c2
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-10-05 17:11:50 +0100

    created Encapsulation example

--- oop_principles.py
+++ oop_principles.py
@@ -19,3 +19,38 @@ class Dog(Animal):
 class Cat(Animal):
   def roar(self):
     return "meow, meow..."
+  
+
+dog = Dog(name = "Max")
+cat = Cat(name = "Juliana")
+
+print(f"\nExamples Polymorphism")
+animals = [dog, cat]
+
+for animal in animals:
+  print(f"{animal.name} roar {animal.roar()}")
+
+
+# Encapsulation
+print(f"\nExamples Encapsulation")
+class BankAccount():
+  def __init__(self, balance) -> None:
+    self.__balance = balance # private attribute
+
+  def deposit(self, value):
+    if value > 0:
+      self.__balance += value
+
+  def withdraw(self, value):
+    if value > 0 and value <= self.__balance:
+      self.__balance -= value
+
+  def get_balance(self):
+    return self.__balance
+  
+account = BankAccount(balance = 1000)
+print(f"\nAccount Balance: {account.get_balance()}")
+account.deposit(value=100)
+print(f"\nAccount Balance after deposit: {account.get_balance()}")
+account.withdraw(value=500)
+print(f"\nAccount Balance after withdraw: {account.get_balance()}")
